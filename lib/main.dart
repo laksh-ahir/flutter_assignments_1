59df45f59c959ed039350d5ac4499e539304fbf8
laksh-ahir <laxmankambariya@gmail.com>
2024-01-12 15:01:57 +0530

    assignment 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,7 @@ import 'package:flutter_assignments_1/Questions/q_2.dart';
 void main() {
   q_1();
   q_2();
+  // Call the function q_'[question number]'() which you want to see the answer. 
+  // NOTE : replace '[question number]' by assignment's question number
+  // Example : if i want to see the answer of question 10 then call function q_10();  
 }
